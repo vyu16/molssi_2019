7e5d20116e4d3b443c20230c2f44149f002aa819
Victor Yu <wy29@duke.edu>
2019-02-12 11:04:52 -0500

    Add mean function to molssi_math

--- molssi_2019/molssi_math.py
+++ molssi_2019/molssi_math.py
@@ -6,6 +6,34 @@ Handles the primary functions
 """
 
 
+def mean(num_list):
+    """
+    Computes the mean of a list of numbers.
+
+    Parameters
+    ----------
+    num_list : list
+        A list of numbers
+
+    Returns
+    -------
+    mean : float
+        Mean of num_list
+    """
+
+    # Check input is type list
+    if not isinstance(num_list, list):
+        raise TypeError("Invalid input %s - must be type list" % (num_list))
+
+    # Check list has length > 0
+    if len(num_list) == 0:
+        raise ZeroDivisionError("Cannot calculate mean of empty list.")
+
+    mean = sum(num_list) / len(num_list)
+
+    return mean
+
+
 def canvas(with_attribution=True):
     """
     Placeholder function to show example docstring (NumPy format)
