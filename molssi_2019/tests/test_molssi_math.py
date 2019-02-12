bff853516599d483e94c12bd0157232fbb1c3444
Victor Yu <wy29@duke.edu>
2019-02-12 13:36:18 -0500

    Add more tests for the mean function

--- molssi_2019/tests/test_molssi_math.py
+++ molssi_2019/tests/test_molssi_math.py
@@ -8,9 +8,25 @@ import pytest
 import sys
 
 
-def test_mean():
-    num_list = [1, 2, 3, 4, 5]
-    observed = ms.mean(num_list)
+@pytest.fixture
+def num_list_3():
+    return [1, 2, 3, 4, 5]
+
+
+@pytest.mark.parametrize("num_list, expected_mean", [
+    ([1, 2, 3, 4, 5], 3),
+    ([0, 2, 4, 6], 3),
+    ([1, 2, 3, 4], 2.5),
+    (list(range(1, 1000000)), 1000000/2)
+])
+
+
+def test_many(num_list, expected_mean):
+    assert ms.mean(num_list) == expected_mean
+
+
+def test_mean(num_list_3):
+    observed = ms.mean(num_list_3)
     expected = 3
 
     assert observed == expected
