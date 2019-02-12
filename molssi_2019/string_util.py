8fbcbb4d69a3ef86697df77844e387fb4f1f677c
semodi <sebastian-dick@gmx.net>
2019-02-12 11:51:12 -0500

    add title_case function

--- molssi_2019/string_util.py
+++ molssi_2019/string_util.py
@@ -7,4 +7,22 @@ Misc. string processing functions
 
 
 def title_case(sentence):
-    return True
+    """
+    Convert string to title case.
+
+    Title case means thtat the first character of every word is capitalized.
+    otherwise lowercase
+
+    Parameters
+    ---------
+    sentence: string
+        String to be converted to title case
+    """
+    if not isinstance(sentence, str):
+        raise TypeError('sentence {} not a string'.format(sentence))
+
+    if len(sentence) == 0:
+        raise ValueError('len(sentence) is zero')
+
+    return ' '.join([word[0].upper() + word[1:].lower() for word in sentence.split()])
+     
